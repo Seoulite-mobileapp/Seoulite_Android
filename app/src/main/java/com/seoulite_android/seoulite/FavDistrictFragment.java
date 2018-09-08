7f2846f701f149e663e2300780f1c8675d6b7968
unknown <jureaky@gmail.com>
2018-09-08 15:52:42 +0900

    fav page cardview with mock data

--- app/src/main/java/com/seoulite_android/seoulite/FavDistrictFragment.java
+++ app/src/main/java/com/seoulite_android/seoulite/FavDistrictFragment.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //
 ///**
@@ -19,6 +25,8 @@ public class FavDistrictFragment extends Fragment {
 //
 //    private OnFragmentInteractionListener mListener;
 
+    private ArrayList<FavVO> mMockList = new ArrayList<>();
+
     public FavDistrictFragment() {
         // Required empty public constructor
     }
@@ -27,8 +35,20 @@ public class FavDistrictFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fav_district, container, false);
+        View view = inflater.inflate(R.layout.fragment_fav_district, container, false);
+
+        // Push mock data
+        mMockList.add(new FavVO(1, "Nowon-gu", 1, 0, null));
+        mMockList.add(new FavVO(2, "Gangnam-gu", 1, 0, null));
+        mMockList.add(new FavVO(3, "Shit-gu", 1, 0, "Pretty fucking shit !"));
+
+        // Set up the recycler view
+        RecyclerView recyclerView = view.findViewById(R.id.fav_district_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1, GridLayoutManager.VERTICAL, false));
+        FavCardRecyclerViewAdapter adapter = new FavCardRecyclerViewAdapter(mMockList);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 //
 //    // TODO: Rename method, update argument and hook method into UI event
