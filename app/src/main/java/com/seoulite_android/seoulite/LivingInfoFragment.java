c31dc7685decd4699a441304a1c36a4a9a17c5c9
DESKTOP-E9SI76E\김연진 <yeonjinkim1021@gmail.com>
2018-09-21 17:59:30 +0900

    living info revised

--- app/src/main/java/com/seoulite_android/seoulite/LivingInfoFragment.java
+++ app/src/main/java/com/seoulite_android/seoulite/LivingInfoFragment.java
@@ -9,11 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 
 public class LivingInfoFragment extends Fragment {
     MainActivity activity;
+    String distName;
+    //@BindView(R.id.text_living_info_district_name) TextView mLivingInfoDistrictName;
+
 
     @Override
     public void onAttach(Context context) {
@@ -26,6 +31,14 @@ public class LivingInfoFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_living_info, container, false);
 
+        //getting agency id from previous fragment.
+        if(savedInstanceState ==null) {
+            gettingDistName();
+        }
+        TextView mLivingInfoDistrictName = (TextView) view.findViewById(R.id.text_living_info_district_name);
+        mLivingInfoDistrictName.setText(distName);
+
+
         return view;
     }
 
@@ -33,5 +46,14 @@ public class LivingInfoFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         activity=null;
+    }
+
+    private void gettingDistName(){
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            distName = bundle.getString("distName");
+        }else{
+            Toast.makeText(activity, "Can't get data. Try again.", Toast.LENGTH_LONG).show();
+        }
     }
 }
