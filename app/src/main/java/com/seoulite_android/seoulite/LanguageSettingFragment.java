361661bfff53589abd2ea8efc6c8c4a78a170b0f
DESKTOP-E9SI76E\김연진 <yeonjinkim1021@gmail.com>
2018-09-21 17:57:57 +0900

    language setting revised

--- app/src/main/java/com/seoulite_android/seoulite/LanguageSettingFragment.java
+++ app/src/main/java/com/seoulite_android/seoulite/LanguageSettingFragment.java
@@ -1,4 +1,40 @@
 package com.seoulite_android.seoulite;
 
-public class LanguageSettingFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LanguageSettingFragment extends Fragment{
+    MainActivity activity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_language_setting, container, false);
+
+        return view;
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity=null;
+    }
+
 }
